73ad5624046dbc1d709b935f6c0841675b227d84
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2022-10-11 15:19:57 +0530

    Create C_16.c

---
+++ Basic Programs/C_16.c
@@ -0,0 +1,22 @@
+/*
+ * Sum of Digits in C using While Loop (Naive Approach)
+ */
+
+#include <stdio.h>
+ 
+int main(void)//the function takes NO argument
+{
+    int num, sum = 0, rem;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+ 
+    // Keep dividing until the number is not zero
+    while (num != 0)
+    {
+        rem = num % 10;
+        sum = sum + rem;
+        num = num / 10;
+    }
+    printf("Sum of digits of the number is %d", sum);
+    return 0;
+}
