214063e09f00723e6bba2183341593e480caddd7
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2022-10-11 14:10:47 +0530

    Create C_8.c

---
+++ Basic Programs/C_8.c
@@ -0,0 +1,18 @@
+/*
+ * largest of Three Numbers in C using Ternary Operator
+ */
+
+#include <stdio.h>
+ 
+int main(void)
+{
+    int a, b, c;
+    printf("Enter three numbers: \na: ");
+    scanf("%d", &a);
+    printf("b: ");
+    scanf("%d", &b);
+    printf("c: ");
+    scanf("%d", &c);
+    printf("Largest of three numbers is %d", a > b ? (a > c ? a : c) : (b > c ? b : c));
+    return 0;
+}
