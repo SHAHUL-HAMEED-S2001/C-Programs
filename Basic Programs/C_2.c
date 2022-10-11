f34b1895b98b5ddd268d2034a9d392b65b75c6dc
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2022-10-11 14:10:20 +0530

    Create C_2.c

---
+++ Basic Programs/C_2.c
@@ -0,0 +1,12 @@
+/*
+ * check Even or Odd in C using Ternary Operator
+ */
+
+#include <stdio.h>
+void main(){
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+ 
+    (n % 2 == 0) ? printf("%d is even number.", n) : printf("%d is odd number.", n);
+}
