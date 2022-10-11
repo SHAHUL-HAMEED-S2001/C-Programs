68acabf2ce7058ebf3ea0f2463d0bda04e48b052
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2022-10-11 14:09:48 +0530

    Create C_1.c

---
+++ Basic Programs/C_1.c
@@ -0,0 +1,14 @@
+/*
+ * check Even or Odd in C using if-else and Modulus Operator
+ */
+#include <stdio.h>
+void main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+ 
+    if(n % 2 == 0)
+        printf("%d is even number.", n);
+    else
+        printf("%d is odd number.", n);
+}
