8ef10f5fe7a761099c7ad6266c7dff50f75f76ea
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2022-10-12 23:08:09 +0530

    Create C_25.c

---
+++ Basic Programs/C_25.c
@@ -0,0 +1,38 @@
+// takes a grade as input and displays its equivalent description
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+ 
+void main()
+{
+    char remark[15];
+    char grade;
+ 
+    printf("Enter the grade \n");
+    scanf("%c", &grade);
+    /*  lower case letter to upper case */
+    grade = toupper(grade);
+    switch(grade)
+    {
+    case 'S':
+        strcpy(remark, " SUPER");
+        break;
+    case 'A':
+        strcpy(remark, " VERY GOOD");
+        break;
+    case 'B':
+        strcpy(remark, " FAIR");
+        break;
+    case 'Y':
+        strcpy(remark, " ABSENT");
+        break;
+    case 'F':
+        strcpy(remark, " FAILS");
+        break;
+    default :
+        strcpy(remark, "ERROR IN GRADE \n");
+        break;
+    }
+    printf("RESULT  : %s\n", remark);
+}
